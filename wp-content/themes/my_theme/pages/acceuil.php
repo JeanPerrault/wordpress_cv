c1077c3cb3094b794f7c6b6d606c671d100feccb
Jean Perrault <perrault.jean66gmail.com>
2019-03-08 11:49:02 +0100

    -

--- wp-content/themes/my_theme/pages/acceuil.php
+++ wp-content/themes/my_theme/pages/acceuil.php
@@ -5,6 +5,6 @@ Template Name: Acceuil (pages/acceuil.php)
 get_header()
 ?>
 
-C'est ma page d' Acceuil
+
 
 <?php get_footer() ?>
--- wp-content/themes/my_theme/pages/porfolio.php
+++ wp-content/themes/my_theme/pages/porfolio.php
@@ -2,9 +2,40 @@
 /*
 Template Name: portfolio (pages/portfolio.php)
 */
-get_header()
-?>
 
-C'est ma page de Portfolio <br>
+$args = [
+    "post_type" => "portfolio",
+    "post_per_page" => 10,
+];
+
+$query = new WP_query($args);
+
+// var_dump($query);
+
+?>
+<?php get_header() ?>
+
+<?php if($query->have_posts()):?> 
+    <?php while($query->have_posts()): ?>
+        <?php $query->the_post() ?>
+        <article>
+            <h1><?= get_the_title() ?></h1>
+            <div> <a href="<?= get_the_permalink() ?>"> <?= get_the_permalink() ?></a></div>
+            <div><?= get_the_excerpt() ?></div>
+            <div><?= get_the_content() ?></div>
+            <br>
+
+            <a href="<?= get_post_meta( get_the_ID(), "url", true ) ?>" class="btn btn-success">
+                    <?= get_post_meta( get_the_ID(), "url_label", true ) ?>
+                </a>
+        </article>
+        
+    <?php endwhile; ?>
+<?php else:?> 
+    <div class="alert alert-warnings">
+        Aucun post sur cette page
+    </div>
+<?php endif;?> 
+<?php wp_reset_postdata() ?>
 
 <?php get_footer() ?>
